d2c2f4ae00a48ed5e96e2d9700bee9adb1e2fac9
Isha_Sayani123-cmd <ishasayani97@gmail.com>
2021-02-18 23:06:03 +0530

    commiting junit

---
+++ src/main/java/com/valuemomentum/training/testing/JUnitDemo/Calulator.java
@@ -0,0 +1,32 @@
+package com.valuemomentum.training.testing.JUnitDemo;
+
+public class Calulator {
+	
+	public double add(double a,double b)
+	{
+		return a+b;
+	}
+	
+	public double substract(double a,double b)
+	{
+		return a-b;
+	}
+	
+	public double multiply(double a,double b)
+	{
+		return a*b;
+	}
+	public double divide(double a,double b)
+	{
+		if(b==0)
+		{
+			throw new ArithmeticException();
+		}
+		return a/b;
+	}
+
+	public static void main(String[] args) {
+
+	}
+
+}
